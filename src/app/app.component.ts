18d989dfce9281f5d5cc54b7dac091ce0764e212
monabbous <mohamed.nabbous@gmail.com>
2019-09-07 12:05:28 +0200

    spiza view

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,40 @@
 import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
 
+interface GenericItem {
+  name: string;
+  images: any[];
+}
+
+interface Item {
+  id: number;
+  generic_item: GenericItem;
+}
+
+interface PageResponse {
+  data: Item[];
+  meta: any;
+}
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'angular-project';
+  disabled = true;
+  text = '';
+  items: Item[] = [];
+
+  constructor(
+    public http: HttpClient,
+  ) {
+
+  }
+  ngOnInit() {
+    this.http.get<PageResponse>('https://spiza-app.com/api/v2/customerboard/items')
+      .subscribe((response) => {
+        console.log(response);
+        this.items = response.data;
+      });
+  }
 }
